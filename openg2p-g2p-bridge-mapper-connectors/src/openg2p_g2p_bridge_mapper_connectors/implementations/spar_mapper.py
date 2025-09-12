a6eb7197b61d7d510149b41215475748860e78bb
PSNAppZ <thepsnarayanan@gmail.com>
2025-09-12 12:35:34 +0530

    pre-commit fixes

--- openg2p-g2p-bridge-mapper-connectors/src/openg2p_g2p_bridge_mapper_connectors/implementations/spar_mapper.py
+++ openg2p-g2p-bridge-mapper-connectors/src/openg2p_g2p_bridge_mapper_connectors/implementations/spar_mapper.py
@@ -1,20 +1,17 @@
 import logging
-from tkinter import N
+
+from openg2p_g2pconnect_mapper_lib.client import MapperResolveClient
+from openg2p_g2pconnect_mapper_lib.schemas import ResolveRequest, ResolveResponse
 
 from ..config import Settings
 from ..interface.mapper_interface import MapperInterface
-from openg2p_g2pconnect_mapper_lib.schemas import ResolveRequest, ResolveResponse
-from openg2p_g2pconnect_mapper_lib.client import MapperResolveClient
-
 
 _config = Settings.get_config()
 _logger = logging.getLogger("spar_mapper_impl")
 
+
 class SPARMapper(MapperInterface):
-    def resolve(
-        self,
-        resolve_request: ResolveRequest
-    ) -> ResolveResponse | None:
+    def resolve(self, resolve_request: ResolveRequest) -> ResolveResponse | None:
         """
         Resolve the given request from Mapper and return the result.
         """
@@ -26,6 +23,6 @@ class SPARMapper(MapperInterface):
         except Exception as e:
             _logger.error(f"Failed to resolve the request: {e}")
             return None
-        
+
         _logger.info(f"Returning resolve response: {resolve_response}")
         return resolve_response
