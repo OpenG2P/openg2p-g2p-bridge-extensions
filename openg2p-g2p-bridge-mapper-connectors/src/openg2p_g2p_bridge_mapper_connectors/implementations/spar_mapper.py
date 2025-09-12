a06b181817f768bcc148f7dd92b7eb87b70f87bf
PSNAppZ <thepsnarayanan@gmail.com>
2025-09-12 12:26:40 +0530

    Add mapper Mapper Connector and SPAR implementation

---
+++ openg2p-g2p-bridge-mapper-connectors/src/openg2p_g2p_bridge_mapper_connectors/implementations/spar_mapper.py
@@ -0,0 +1,31 @@
+import logging
+from tkinter import N
+
+from ..config import Settings
+from ..interface.mapper_interface import MapperInterface
+from openg2p_g2pconnect_mapper_lib.schemas import ResolveRequest, ResolveResponse
+from openg2p_g2pconnect_mapper_lib.client import MapperResolveClient
+
+
+_config = Settings.get_config()
+_logger = logging.getLogger("spar_mapper_impl")
+
+class SPARMapper(MapperInterface):
+    def resolve(
+        self,
+        resolve_request: ResolveRequest
+    ) -> ResolveResponse | None:
+        """
+        Resolve the given request from Mapper and return the result.
+        """
+        _logger.info(f"Received resolve request: {resolve_request}")
+        resolve_client = MapperResolveClient.get_component()
+        try:
+            resolve_response = resolve_client.resolve_request(resolve_request)
+            _logger.info("Resolve request completed successfully")
+        except Exception as e:
+            _logger.error(f"Failed to resolve the request: {e}")
+            return None
+        
+        _logger.info(f"Returning resolve response: {resolve_response}")
+        return resolve_response
